049b164a4ed22b9b4ebac740d46c734baed2339e
El-Razin Jo <elrazinmjo@gmail.com>
2025-07-28 10:21:01 +0800

    Adds redirect when no personality data

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
 import { usePersonality } from "@/context/usePersonality";
+import { useRouter } from "next/navigation";
 import { FC } from "react";
 
 const ResultPage: FC = () => {
+  const router = useRouter();
+
   const { personality } = usePersonality();
+
+  if (!personality) {
+    router.push("/result");
+  }
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 py-12 px-4 sm:px-6 lg:px-8">
